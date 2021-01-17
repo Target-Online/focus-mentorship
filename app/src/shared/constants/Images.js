695d63a35413151ff0ad90292cfc2d2e28d52879
MKHSKH003 <mkhskh003@myuct.ac.za>
2021-01-17 03:38:29 +0200

    uncommitted changes.

--- app/src/shared/constants/Images.js
+++ app/src/shared/constants/Images.js
@@ -1,16 +1,14 @@
 export const sliderImages = [
   require('../assets/images/slider-images/0A.jpg'),
+  require('../assets/images/slider-images/1.jpg'),
+  require('../assets/images/slider-images/1.png'),
   require('../assets/images/slider-images/01.jpeg'),
-  require('../assets/images/slider-images/02.jpeg'),
   require('../assets/images/slider-images/03.jpeg'),
-  require('../assets/images/slider-images/1.jpeg'),
   require('../assets/images/slider-images/class.jpeg'),
   require('../assets/images/slider-images/teaching.jpg'),
   require('../assets/images/slider-images/tutor.jpeg'),
-  require('../assets/images/slider-images/2.jpeg'),
   require('../assets/images/slider-images/3.jpeg'),
   require('../assets/images/slider-images/4.jpeg'),
-  require('../assets/images/slider-images/5.jpeg')
 ]
 
 export const user = require('../assets/images/user.png')
